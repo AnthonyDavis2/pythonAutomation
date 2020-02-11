247f9fa9b7781f7b38cb483c29df5c070380ed3c
Anthony <40148784+AnthonyDavis2@users.noreply.github.com>
2020-02-11 12:47:58 -0500

    Update addContact.py

--- addContact.py
+++ addContact.py
@@ -3,12 +3,12 @@ import time
 
 firstName = ''
 lastName = ''
-email = 'jrdavisa'
-passw = '91792Cali'
+email = ''
+passw = ''
 
 url = 'https://accounts.google.com/signin/v2/identifier?passive=1209600&continue=https%3A%2F%2Fcontacts.google.com%2F&followup=https%3A%2F%2Fcontacts.google.com%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
 
-driver = webdriver.Chrome("/Users/nkosiakbar/Downloads/chromedriver")
+driver = webdriver.Chrome("/Users/Downloads/chromedriver")
 
 driver.get(url)
 
